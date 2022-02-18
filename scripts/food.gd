d2e725dfa384ba3ee94056e44bdf00e8e3d78169
Emememssss <74091502+Emememssss@users.noreply.github.com>
2022-02-18 18:06:21 +0800

    Revert "Changed food scenes"
    This reverts commit dd8b23781ec8985742d7197d18f58e2e706d15e1.

--- scripts/food.gd
+++ scripts/food.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 var sprite_food_width
 signal food_collected
@@ -8,22 +8,22 @@ func _ready():
 	sprite_food_width = $Sprite.texture.get_size().x * scale.x
 
 
-
 func _on_VisibilityNotifier2D_screen_entered():
 	Signals.emit_signal("create_new_item_food")
-	
 
-func _on_Bread_body_entered(body):
+
+func _on_food_body_entered(body):
 	if body.is_in_group("Player"):
 		$GoodFoodSound.play()
-		collected2()
-		
-func collected2():
+		collected()
+
+func collected():
 	emit_signal("food_collected")
 	Global.food_collected = true
 	Global.foods += 1
-	#Global.ADD_SPEED -= 5
+	#Global.ADD_SPEED += 50
 	$Sprite.set_visible(false)
+	#queue_free()
 
 func _on_GoodFoodSound_finished():
 	queue_free()
--- scripts/food3.gd
+++ scripts/food3.gd
@@ -7,7 +7,7 @@ func _ready():
 	sprite_food_width = $Sprite.texture.get_size().x * scale.x
 
 
-func _on_Tomato_body_entered(body):
+func _on_rice_body_entered(body):
 	if body.is_in_group("Player"):
 		$GoodFoodSound.play()
 		collected()
@@ -22,3 +22,7 @@ func collected():
 
 func _on_VisibilityNotifier2D_screen_entered():
 	Signals.emit_signal("create_new_item_food")
+
+
+func _on_GoodFoodSound_finished():
+	queue_free()
