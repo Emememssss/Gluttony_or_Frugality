280d2e260909030d1af631fbaca4b007c092a4ae
Emememssss <74091502+Emememssss@users.noreply.github.com>
2022-02-18 18:34:11 +0800

    up

--- scripts/slurbar.gd
+++ scripts/slurbar.gd
@@ -8,7 +8,7 @@ func _ready():
 	sb.set_percent_visible(70)
 	sb.set_value(0)
 	sb.set_size(Vector2(130,25))
-	sb.set_position(Vector2(-100,100))
+	sb.set_position(Vector2(95,138))
 	var style = StyleBoxFlat.new()
 	style.set_bg_color(Color.yellow)
 	sb.set("custom_styles/fg" , style)
@@ -20,30 +20,29 @@ func _ready():
 	
 	
 func _process(_delta):
-	var valu = get_node("sb").get_value()
+	#var valu = get_node("sb").get_value()
+	var valu
+	valu =  Global.badfoods*120 - Global.badfoods
+	
+	get_node("sb").set_value(valu)
 	if valu > 60:
 		var stylsbox = .get_node("sb").get("custom_styles/fg")
 		stylsbox.set_bg_color(Color.red)
+		Global.ADD_SPEED = -60
 		
 	elif valu > 30 and valu <= 60:
 		var stylsbox = .get_node("sb").get("custom_styles/fg")
 		stylsbox.set_bg_color(Color.orange)
+		Global.ADD_SPEED = -40
 		
 	elif valu <= 30:
 		var stylsbox = .get_node("sb").get("custom_styles/fg")
 		stylsbox.set_bg_color(Color.green)
+		if valu != 0:
+			Global.ADD_SPEED = -200
 
 
-func _on_Timer_timeout():
-	var valu
-	Slur -= 1
-	
-	if Slur >= 100:
-		Slur = 100
-		get_node("sb").set_value(Slur)
-		
-	elif Slur < 100:
-		get_node("sb").set_value(Slur)
+
 		
 	#elif Slur <= 0:
 	#	get_tree().change_scene("res://scenes/User Interface/Death_Screen.tscn")
